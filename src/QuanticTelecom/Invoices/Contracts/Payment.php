eac37956407f5804d54b1ce9ffbf7d7d8fc61f04
Thibaud <thibaud@dauce.fr>
2015-03-03 12:02:36 +0100

    feat: add a payment in the invoice
    Create a new Payment contract
    Add setter and getter for a payment variable
    Add a isPaid method to test the payment

---
+++ src/QuanticTelecom/Invoices/Contracts/Payment.php
@@ -0,0 +1,16 @@
+<?php namespace QuanticTelecom\Invoices\Contracts;
+
+use Carbon\Carbon;
+
+interface Payment {
+
+    /**
+     * @return string
+     */
+    public function getPaymentName();
+
+    /**
+     * @return Carbon
+     */
+    public function getPaymentDate();
+}
