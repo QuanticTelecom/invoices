9f920c8a9bc365f4a8da799337c4a950b14f70d5
Thibaud Dauce <thibaud@dauce.fr>
2015-05-15 23:41:02 +0200

    docs: add dockblocks description in contracts

--- src/QuanticTelecom/Invoices/Contracts/ItemInterface.php
+++ src/QuanticTelecom/Invoices/Contracts/ItemInterface.php
@@ -5,31 +5,43 @@ namespace QuanticTelecom\Invoices\Contracts;
 interface ItemInterface
 {
     /**
+     * Get the item name.
+     *
      * @return string
      */
     public function getItemName();
 
     /**
+     * Get the item quantity.
+     *
      * @return int
      */
     public function getItemQuantity();
 
     /**
+     * Get the unit price of the item with taxes.
+     *
      * @return float
      */
     public function getItemIncludingTaxUnitPrice();
 
     /**
+     * Get the total price of the item with taxes.
+     *
      * @return float
      */
     public function getItemIncludingTaxTotalPrice();
 
     /**
+     * Get the unit price of the item without taxes.
+     *
      * @return float
      */
     public function getItemExcludingTaxUnitPrice();
 
     /**
+     * Get the total price of the item without taxes.
+     *
      * @return float
      */
     public function getItemExcludingTaxTotalPrice();
--- src/QuanticTelecom/Invoices/Contracts/PaymentInterface.php
+++ src/QuanticTelecom/Invoices/Contracts/PaymentInterface.php
@@ -7,11 +7,15 @@ use Carbon\Carbon;
 interface PaymentInterface
 {
     /**
+     * Get the name of the payment.
+     *
      * @return string
      */
     public function getPaymentName();
 
     /**
+     * Get the date of the payment.
+     *
      * @return Carbon
      */
     public function getPaymentDate();
